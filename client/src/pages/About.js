266a156354a012b8880c4de6a0120d137ec872a8
hsy822 <hyunsy822@gmail.com>
2020-02-03 15:42:00 +0900

    update about section

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -12,7 +12,8 @@ class About extends Component {
         <iframe
           style={style}
           title="docs"
-          src="https://docs.google.com/document/d/e/2PACX-1vSR9CMokWoRffjp1D8MV0q-85jM1Lw3l_-QP_cxiUEinwRsRYgMk7WWk_UCuvh51udFVYZ0UNVDc2GS/pub?embedded=true"
+          // src="https://docs.google.com/document/d/e/2PACX-1vSR9CMokWoRffjp1D8MV0q-85jM1Lw3l_-QP_cxiUEinwRsRYgMk7WWk_UCuvh51udFVYZ0UNVDc2GS/pub?embedded=true"
+          src="https://docs.google.com/document/d/e/2PACX-1vROkuBkGa_AMdHxvZYUNqLAoQ84IXm3-LIRSafvQtgd4aeL2UsKOafYS3hC551vRhAJO9NmC5z8gUzb/pub?embedded=true"
         />
       </div>
     );
