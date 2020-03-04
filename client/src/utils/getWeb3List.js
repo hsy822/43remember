d4f1d3564b5575bae9a7f0513bde2b0d647b41e2
hsy822 <hyunsy822@gmail.com>
2020-03-05 07:24:59 +0900

    fix mainnet provider address

--- client/src/utils/getWeb3List.js
+++ client/src/utils/getWeb3List.js
@@ -3,7 +3,7 @@ import Web3 from "web3";
 const getWeb3List = () =>
   new Promise(async (resolve, reject) => {
     const provider = new Web3.providers.HttpProvider(
-      "https://mainnet.infura.io/"
+      "https://mainnet.infura.io/v3/38a0771a75c4474cb0f0703f6b053c2d"
     );
     const web3 = new Web3(provider);
     console.log("No web3 instance injected, using Local web3.");
